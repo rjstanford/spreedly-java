7fcd30e7d2543d9947ffd01f823de2c1dd8e0d9e
Alex Ehrnschwender <alexanderscott46@gmail.com>
2015-09-15 23:55:42 -0700

    adds more tests to SpreedlyTest suite

--- src/test/java/cc/protea/spreedly/SpreedlyTest.java
+++ src/test/java/cc/protea/spreedly/SpreedlyTest.java
@@ -2,6 +2,9 @@ package cc.protea.spreedly;
 
 import java.util.List;
 
+import cc.protea.spreedly.model.SpreedlyGatewayAccount;
+import cc.protea.spreedly.model.SpreedlyGatewayAccountState;
+import cc.protea.spreedly.model.SpreedlyGatewayCredential;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +15,7 @@ import cc.protea.spreedly.model.SpreedlyGatewayProvider;
 public class SpreedlyTest {
 
 	Spreedly spreedly;
+	SpreedlyGatewayAccount testAccount;
 
 	@Before
 	public void before() {
@@ -27,6 +31,58 @@ public class SpreedlyTest {
     	Assert.assertNotNull(list);
     	Assert.assertTrue(! list.isEmpty());
     }
+
+	@Test
+	public void testCreate() throws Exception {
+		if (spreedly == null) {
+			return;
+		}
+		SpreedlyGatewayAccount account = new SpreedlyGatewayAccount();
+		account.gatewayType = "sage";
+		SpreedlyGatewayCredential c = new SpreedlyGatewayCredential();
+		c.name = "merchant_id_number";
+		c.value = "12345";
+		account.credentials.add(c);
+
+		final SpreedlyGatewayAccount createdAccount = spreedly.create(account);
+		Assert.assertNotNull(createdAccount);
+		Assert.assertTrue(createdAccount.getToken() == "sage");
+		Assert.assertTrue(createdAccount.getGatewayType() == "sage");
+		Assert.assertTrue(!createdAccount.getCredentials().isEmpty());
+		Assert.assertTrue(createdAccount.getCredentials().get(0).getName() == "merchant_id_number");
+		Assert.assertTrue(createdAccount.getCredentials().get(0).getValue() == "12345");
+
+		testAccount = createdAccount;
+	}
+
+	@Test
+	public void testRedact() throws Exception {
+		if (spreedly == null) {
+			return;
+		}
+
+		SpreedlyGatewayAccount redactedAccount = spreedly.redact(testAccount);
+		Assert.assertTrue(redactedAccount.getState() == SpreedlyGatewayAccountState.REDACTED);
+	}
+
+	@Test
+	public void testRetain() throws Exception {
+		if (spreedly == null) {
+			return;
+		}
+		SpreedlyGatewayAccount retainedAccount = spreedly.retain(testAccount);
+		Assert.assertTrue(retainedAccount.getState() == SpreedlyGatewayAccountState.RETAINED);
+	}
+
+	@Test
+	public void testGetGatewayAccounts() throws Exception {
+		if (spreedly == null) {
+			return;
+		}
+		final List<SpreedlyGatewayAccount> list = spreedly.listGatewayAccounts();
+		Assert.assertNotNull(list);
+		Assert.assertTrue(! list.isEmpty());
+	}
 
     public static Spreedly getSpreedly() {
 		final String environmentKey = SpreedlyTest.getEnvironmentKey();
