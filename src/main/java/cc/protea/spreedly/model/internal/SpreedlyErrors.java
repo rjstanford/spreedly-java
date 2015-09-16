2b615cd78e8295de8a01fa9efb756e6f4baa7f34
Alex Ehrnschwender <alexanderscott46@gmail.com>
2015-09-16 16:20:42 -0700

    fix SpreedlyError xml mapping

--- src/main/java/cc/protea/spreedly/model/internal/SpreedlyErrors.java
+++ src/main/java/cc/protea/spreedly/model/internal/SpreedlyErrors.java
@@ -1,20 +1,12 @@
 package cc.protea.spreedly.model.internal;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlValue;
 
 @XmlRootElement(name = "errors")
-static class SpreedlyErrors {
+public class SpreedlyErrors {
     @XmlElement(name = "error") public List<SpreedlyError> errors = new ArrayList<SpreedlyError>();
-}
-
-static class SpreedlyError {
-    @XmlValue public String error;
-    @XmlAttribute(name = "key") public String key;
-    @XmlAttribute(name = "attribute") public String attribute;
 }
 
