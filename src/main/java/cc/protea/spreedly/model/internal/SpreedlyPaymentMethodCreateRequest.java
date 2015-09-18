f0305776a8e2fa36deb9465dd08ada7073e0ba59
Alex Ehrnschwender <alexanderscott46@gmail.com>
2015-09-18 04:04:01 -0700

    fix serialization for SpreedlyPaymentMethodCreateRequest

--- src/main/java/cc/protea/spreedly/model/internal/SpreedlyPaymentMethodCreateRequest.java
+++ src/main/java/cc/protea/spreedly/model/internal/SpreedlyPaymentMethodCreateRequest.java
@@ -1,9 +1,11 @@
 package cc.protea.spreedly.model.internal;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import cc.protea.spreedly.model.SpreedlyCreditCard;
 
+@XmlRootElement(name = "payment_method")
 public class SpreedlyPaymentMethodCreateRequest {
 
 	@XmlElement(name = "credit_card") public SpreedlyCreditCard creditCard;
