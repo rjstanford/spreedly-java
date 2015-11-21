90a081f77e91c95cffa2a32f6be84b5b3044e4f4
Jason Swenski <jswenski@gmail.org>
2015-11-20 21:46:38 -0700

    Adding root node annotations so Spreedly.create(SpreedlyCreditCard) works correctly

--- src/main/java/cc/protea/spreedly/model/internal/SpreedlyPaymentMethodCreateRequest.java
+++ src/main/java/cc/protea/spreedly/model/internal/SpreedlyPaymentMethodCreateRequest.java
@@ -3,7 +3,9 @@ package cc.protea.spreedly.model.internal;
 import javax.xml.bind.annotation.XmlElement;
 
 import cc.protea.spreedly.model.SpreedlyCreditCard;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name = "payment_method")
 public class SpreedlyPaymentMethodCreateRequest {
 
 	@XmlElement(name = "credit_card") public SpreedlyCreditCard creditCard;
