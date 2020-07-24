3a192774e23462d6dd1035785a80d83d51761930
alex <alex@bizzabo.com>
2020-07-24 16:46:17 +0300

    change to entry

--- src/main/java/cc/protea/spreedly/model/SpreedlyCompleteRequest.java
+++ src/main/java/cc/protea/spreedly/model/SpreedlyCompleteRequest.java
@@ -1,17 +1,63 @@
 
 package cc.protea.spreedly.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.HashMap;
-import java.util.Map;
+import javax.xml.bind.annotation.XmlType;
 
-
-@XmlRootElement(name = "context")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class SpreedlyCompleteRequest extends HashMap<String, Object>
-{
+@XmlType(name = "", propOrder = {
+        "entry"
+})
+@XmlRootElement(name = "context")
+public class SpreedlyCompleteRequest {
+
+    protected List<SpreedlyCompleteRequest.Entry> entry;
+
+
+    public List<SpreedlyCompleteRequest.Entry> getEntry() {
+        if (entry == null) {
+            entry = new ArrayList<SpreedlyCompleteRequest.Entry>();
+        }
+        return this.entry;
+    }
+
+    public void setEntry(List<Entry> entry) {
+        this.entry = entry;
+    }
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+            "key",
+            "value"
+    })
+    public static class Entry {
+
+        @XmlElement(required = true)
+        protected String key;
+        @XmlElement(required = true)
+        protected Object value;
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String value) {
+            this.key = value;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
+    }
 
 }
